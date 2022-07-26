2876c8f9986cfe434ca5d183f95f606e058023c4
Your Nameievukas256 <you@example.comsibailaite@gmail.com>
2022-07-26 20:10:21 +0300

    js create checkbox

--- toDoApp/script.js
+++ toDoApp/script.js
@@ -10,9 +10,13 @@ class Item {
     const divItem = document.createElement(`div`);
     divItem.className = `to-do-item`;
     divItem.textContent = tekstas;
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "to-do-checkbox3";
+    divItem.prepend(checkbox);
     document.getElementById(`toDoContainer`).append(divItem);
   }
 }
 
 new Item(`isiusti laiska`);
-new Item(`pavalgyti`);
+//new Item(`pavalgyti`, checkbox);
