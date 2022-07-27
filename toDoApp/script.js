e6a93ffc711a7fff911cf98f44aff20327f485fb
SigisLev <sigitaslev@gmail.com>
2022-07-27 19:01:08 +0300

    added some functionality

--- toDoApp/script.js
+++ toDoApp/script.js
@@ -6,17 +6,34 @@
 }*/
 
 class Item {
-  constructor(tekstas) {
+  constructor(text, index) {
+    this.text = text;
+    this.index = index;
+
     const divItem = document.createElement(`div`);
     divItem.className = `to-do-item`;
-    divItem.textContent = tekstas;
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.id = "to-do-checkbox3";
+    divItem.textContent = this.text;
+    divItem.id = 'to-do-item' + index;
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'to-do-checkbox' + index;
+
     divItem.prepend(checkbox);
     document.getElementById(`toDoContainer`).append(divItem);
   }
 }
+document.getElementById('input-field').addEventListener('keydown', (e) => {
+  if (e.code === 'Enter') {
+    const elem = e.target.value;
 
-new Item(`isiusti laiska`);
-//new Item(`pavalgyti`, checkbox);
+    console.log(e.code);
+    const newItem = new Item(elem, lastIndex++);
+  }
+});
+const arr = [];
+let lastIndex = 0;
+
+new Item(`isiusti laiska`, lastIndex++);
+
+new Item(`pavalgyti`, lastIndex++);
