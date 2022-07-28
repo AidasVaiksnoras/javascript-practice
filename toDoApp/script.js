69b52cbd56aa2955ff5d57b2f72913f2610ec6ca
keistas3 <k.strimaitis@gmail.com>
2022-07-28 19:53:51 +0300

    Added Local Storage For items

--- toDoApp/script.js
+++ toDoApp/script.js
@@ -1,10 +1,3 @@
-/*class Rectangle {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
-  }
-}*/
-
 class Item {
   constructor(text, index) {
     this.text = text;
@@ -13,11 +6,11 @@ class Item {
     const divItem = document.createElement(`div`);
     divItem.className = `to-do-item`;
     divItem.textContent = this.text;
-    divItem.id = 'to-do-item' + index;
+    divItem.id = "to-do-item" + index;
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.id = 'to-do-checkbox' + index;
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "to-do-checkbox" + index;
 
     divItem.prepend(checkbox);
     document.getElementById(`toDoContainer`).append(divItem);
@@ -26,20 +19,24 @@ class Item {
 
 class TodoControler {
   constructor() {
-    this.items = []
-    this.lastIndex = 0
-    document.getElementById('input-field').addEventListener('keydown', (e) => {
-      if (e.code === 'Enter') {
+    this.items = [];
+    this.lastIndex = 0;
+    const itemsString = localStorage.getItem(`items`);
+    const cacheItems = JSON.parse(itemsString);
+    for (let i = 0; i < cacheItems.length; i++) {
+      const newItem = new Item(cacheItems[i].text, cacheItems[i].index);
+      this.items.push(newItem);
+    }
+    document.getElementById("input-field").addEventListener("keydown", (e) => {
+      if (e.code === "Enter") {
         const elem = e.target.value;
         const newItem = new Item(elem, this.lastIndex++);
-        document.getElementById('input-field').value = ''
+        document.getElementById("input-field").value = "";
         this.items.push(newItem);
+        localStorage.setItem(`items`, JSON.stringify(this.items));
       }
     });
-
   }
 }
 
 new TodoControler();
-
-
