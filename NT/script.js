278ba2fe13d81b2570149afcbce6a37e54a6ef31
Petriukas <aistis.jampolskisu@gmail.com>
2022-08-11 19:58:46 +0300

    filtered Vilnius

--- NT/script.js
+++ NT/script.js
@@ -1,23 +1,41 @@
+const state = { properties: [] };
 fetch("https://robust-safe-crafter.glitch.me/")
   .then((Response) => Response.json())
-  .then((data) => console.log(data));
-function drawCards() {
-  const card = document.createElement("div");
-  const divImage = document.createElement("div");
-  const cardImg = document.createElement("img");
-  const divPara = document.createElement("div");
-  const paragraph1 = document.createElement("p");
-  const paragraph2 = document.createElement("p");
-  const paragraph3 = document.createElement("p");
-  card.append(divImage, divPara);
-  divImage.append(cardImg);
-  divPara.append(paragraph1, paragraph2, paragraph3);
-  document.querySelector(".card-wrapper").append(card);
-  card.className = "card";
-  divImage.className = "img";
-  divPara.className = "card-footer";
-  paragraph1.className = "price";
-  paragraph2.className = "city";
-  paragraph3.className = "info";
+  .then((data) => {
+    drawCards(data);
+    state.properties = data;
+  });
+function drawCards(properties) {
+  const cardWrapper = document.querySelector(".card-wrapper");
+  cardWrapper.innerHTML = "";
+  properties.forEach((element) => {
+    const card = document.createElement("div");
+    const divImage = document.createElement("div");
+    const cardImg = document.createElement("img");
+    const divPara = document.createElement("div");
+    const paragraph1 = document.createElement("p");
+    const paragraph2 = document.createElement("p");
+    const paragraph3 = document.createElement("p");
+    card.append(divImage, divPara);
+    divImage.append(cardImg);
+    divPara.append(paragraph1, paragraph2, paragraph3);
+    cardWrapper.append(card);
+
+    card.className = "card";
+    divImage.className = "img";
+    divPara.className = "card-footer";
+    paragraph1.className = "price";
+    paragraph2.className = "city";
+    paragraph3.className = "info";
+    paragraph1.textContent = element.price;
+    paragraph2.textContent = element.city;
+    paragraph3.textContent = element.description;
+    cardImg.src = element.image;
+  });
 }
-drawCards();
+document.getElementById("Vno").addEventListener("click", (e) => {
+  const FilterProperties = state.properties.filter(
+    (element) => element.city === "Vilnius"
+  );
+  drawCards(FilterProperties);
+});
